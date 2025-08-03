b08ca4e5c14994058308cd231d2f183c2ccce895
HuynhKhanh1402 <khanh@duck.com>
2025-08-04 01:27:42 +0700

    feat: implement login page with input component and authentication layout

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { useNavigate } from 'react-router-dom'
+import Input from '../../components/inputs/Input'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
+
+  const navigate = useNavigate();
+
+  // Handle Login Form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault()
+  }
+
+
+
   return (
-    <div>Login</div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          Please enter your details to login
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label='Email Address'
+            placeholder='example@mail.com'
+            type='text'
+          />
+        </form>
+      </div>
+    </AuthLayout>
   )
 }
 
