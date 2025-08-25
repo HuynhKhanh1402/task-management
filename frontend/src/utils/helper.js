62094832c922eb76ebb96b654fc82291f4bdbe21
HuynhKhanh1402 <khanh@duck.com>
2025-08-25 10:56:59 +0700

    feat: implement Dashboard page with data fetching, InfoCard component, and dynamic SideMenu

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -2,3 +2,11 @@ export const validateEmail = email => {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(email);
 };
+
+export const addThousandsSeparator = num => {
+  if (num == null || isNaN(num)) return '';
+
+  const [integerPart, fractionalPart] = num.toString().split('.');
+  const formattedIntegerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return fractionalPart ? `${formattedIntegerPart}.${fractionalPart}` : formattedIntegerPart;
+};
